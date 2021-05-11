6e4b9cbd76d5ccb4e02d627e39c94988152789b8
Dahmane Seghier <dahmane.seghier@gmail.com>
2021-05-11 21:26:40 +0200

    Added entry js file (thats starts the nuxt app)

---
+++ entry.js
@@ -0,0 +1,22 @@
+const { loadNuxt, build } = require('nuxt')
+const http = require('http')
+
+// Check if we need to run Nuxt in development mode
+const isDev = process.env.NODE_ENV !== 'production'
+
+// Get a ready to use Nuxt instance
+loadNuxt(isDev ? 'dev' : 'start').then(nuxt => {
+
+    // Enable live build & reloading on dev
+    if (isDev) {
+        build(nuxt)
+    }
+    
+    // We can use `nuxt.render(req, res)` or `nuxt.renderRoute(route, context)`
+    
+    
+    http.createServer(function (req, res) {
+        nuxt.render(req, res)
+    }).listen(80);
+
+})
